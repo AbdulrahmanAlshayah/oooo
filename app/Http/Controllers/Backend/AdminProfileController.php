470bb622fd458eb100639c780975325bb325ef0d
Abdulrhman-Coder <abdulrhmanalshayay@gmail.com>
2022-10-02 13:55:05 +0300

    5.6.1 Admin Profile & image Update

--- app/Http/Controllers/Backend/AdminProfileController.php
+++ app/Http/Controllers/Backend/AdminProfileController.php
@@ -9,5 +9,13 @@ use Illuminate\Http\Request;
 ##############php artisan maek:controller Backend/AdminProfileController############
 class AdminProfileController extends Controller
 {
-    //
+    public function AdminProfile(){
+        $adminData = Admin::find(1);
+        return view('admin.admin_profile_view',compact('adminData'));
+
+
+
+
+    
+    }
 }
