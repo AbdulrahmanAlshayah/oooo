101f0094b263969cacb8075bbee9168c5352a9b6
Abdulrhman-Coder <abdulrhmanalshayay@gmail.com>
2022-10-02 17:32:12 +0300

    5.8.1 Admin Profile & image Update part3

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,8 @@
 @extends('admin.admin_master')
 @section('admin')
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 <div class="container-full">
 {{-- ###########نسختا من صفحة formvalidation########## --}}
     <!-- Main content -->
@@ -50,12 +53,12 @@
                                 <div class="form-group">
                                     <h5>Profile Image <span class="text-danger">*</span></h5>
                                     <div class="controls">
-                                        <input type="file" name="profile_photo_path" class="form-control" required=""> <div class="help-block"></div></div>
+                                        <input type="file" name="profile_photo_path" class="form-control" required="" id="image"> <div class="help-block"></div></div>
                                 </div>
                             </div>{{-- end col md 6 --}}
 
                             <div class="col-md-6">
-                                <img src="{{(!empty($editData->profile_photo_path))?url('upload/admin_images/'.$editData->profile_photo_path):url('upload/no_image.jpg')}}" style="width: 100px; height: 100px;">
+                                <img id="showImage" src="{{(!empty($editData->profile_photo_path))?url('upload/admin_images/'.$editData->profile_photo_path):url('upload/no_image.jpg')}}" style="width: 100px; height: 100px;">
                             </div>{{-- end col md 6 --}}
 
                         </div>{{-- end row --}}
@@ -79,4 +82,19 @@
        </section>
     <!-- /.content -->
   </div>
+
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+    $('#image').change(function(e){
+        var reader = new FileReader();
+        reader.onload = function(e){
+            $('#showImage').attr('src',e.target.result);
+        }
+        reader.readAsDataURL(e.target.files['0']);
+    });
+});
+
+</script>
   @endsection
