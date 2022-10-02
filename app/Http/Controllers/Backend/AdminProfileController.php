200f21a0886b9912a68610cffe98a4901d0fa50d
Abdulrhman-Coder <abdulrhmanalshayay@gmail.com>
2022-10-02 13:53:58 +0300

    5.6.1 Admin Profile & image Update

---
+++ app/Http/Controllers/Backend/AdminProfileController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+###########################لإنشاء كونترولار داخل مجلد ###################################
+##############php artisan maek:controller Backend/AdminProfileController############
+class AdminProfileController extends Controller
+{
+    //
+}
