bf0e63b0c81c30bbbe11f351c0564e59049961cc
Abdulrhman-Coder <abdulrhmanalshayay@gmail.com>
2022-10-03 17:37:08 +0300

    7.2.1  User Profile Design Part 2

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\User;
 
 class IndexController extends Controller
 {
@@ -15,5 +16,11 @@ class IndexController extends Controller
     public function UserLogout(){
         Auth::logout();
         return Redirect()->route('login');
+    }
+
+    public function Userprofile(){
+        $id = Auth::user()->id;
+        $user = User::find($id);
+        return view('frontend.profile.user_profile');
     }
 }
