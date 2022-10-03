60c6e8671b128c5a981b8a703bc8fed2abd90d52
Abdulrhman-Coder <abdulrhmanalshayay@gmail.com>
2022-10-03 07:42:18 +0300

    5.10.1 Adding Toster In For View Message

--- app/Http/Controllers/Backend/AdminProfileController.php
+++ app/Http/Controllers/Backend/AdminProfileController.php
@@ -34,6 +34,11 @@ class AdminProfileController extends Controller
                 $data['profile_photo_path'] = $filename;
             }
             $data->save();
-            return redirect()->route('admin.profile');
+
+            $notification = array(
+                'message' => 'Admin Profile Update successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->route('admin.profile')->with($notification);
             }// end method
 }
