badda7e06dd862ddf8f3e6863863856749557160
Abdulrhman-Coder <abdulrhmanalshayay@gmail.com>
2022-10-19 12:09:08 +0300

    16.6.1 Product Tags Paginate

--- resources/views/frontend/tags/tags_view.blade.php
+++ resources/views/frontend/tags/tags_view.blade.php
@@ -261,17 +261,7 @@ $subcategories = App\Models\SubCategory::where('category_id',$category->id)->ord
             </div>
             <!-- /.col -->
             <div class="col col-sm-6 col-md-4 text-right">
-              <div class="pagination-container">
-                <ul class="list-inline list-unstyled">
-                  <li class="prev"><a href="#"><i class="fa fa-angle-left"></i></a></li>
-                  <li><a href="#">1</a></li>
-                  <li class="active"><a href="#">2</a></li>
-                  <li><a href="#">3</a></li>
-                  <li><a href="#">4</a></li>
-                  <li class="next"><a href="#"><i class="fa fa-angle-right"></i></a></li>
-                </ul>
-                <!-- /.list-inline --> 
-              </div>
+              
               <!-- /.pagination-container --> </div>
             <!-- /.col --> 
           </div>
@@ -475,12 +465,7 @@ $subcategories = App\Models\SubCategory::where('category_id',$category->id)->ord
             <div class="text-right">
               <div class="pagination-container">
                 <ul class="list-inline list-unstyled">
-                  <li class="prev"><a href="#"><i class="fa fa-angle-left"></i></a></li>
-                  <li><a href="#">1</a></li>
-                  <li class="active"><a href="#">2</a></li>
-                  <li><a href="#">3</a></li>
-                  <li><a href="#">4</a></li>
-                  <li class="next"><a href="#"><i class="fa fa-angle-right"></i></a></li>
+                    {{ $products->links() }}
                 </ul>
                 <!-- /.list-inline --> 
               </div>
