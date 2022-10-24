19a439a73e18557a8369c1f56cd96a1085e0bedb
Abdulrhman-Coder <abdulrhmanalshayay@gmail.com>
2022-10-24 07:10:43 +0300

    24.2.1 Coupon Apply With Ajax Part 2

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -10,6 +10,9 @@ use Auth;
 use App\Models\Wishlist;
 use Carbon\Carbon;
 
+use App\Models\Coupon;
+use Illuminate\Support\Facades\Session;
+
 class CartController extends Controller
 {
     public function AddToCart(Request $request, $id){
@@ -106,9 +109,27 @@ class CartController extends Controller
 
 
         public function CouponApply(Request $request){
-
-        }
+            $coupon = Coupon::where('coupon_name',$request->coupon_name)->where('coupon_validity','>=',Carbon::now()->format('Y-m-d'))->first();
+            if ($coupon) {
     
+                Session::put('coupon',[
+                    'coupon_name' => $coupon->coupon_name,
+                    'coupon_discount' => $coupon->coupon_discount,
+                    'discount_amount' => round(Cart::total() * $coupon->coupon_discount/100), 
+                    'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100)  
+                ]);
+
+                return response()->json(array(
+    
+                    'success' => 'Coupon Applied Successfully'
+                ));
+    
+            }else{
+                return response()->json(['error' => 'Invalid Coupon']);
+            }
+    
+        } // end method 
+            
     
     
     
