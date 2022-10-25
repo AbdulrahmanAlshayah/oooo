d352b4cb76e778fc9bed10f8454271b7b873ecaa
Abdulrhman-Coder <abdulrhmanalshayay@gmail.com>
2022-10-25 09:15:20 +0300

    25.1. Checkout Page Design Part 1

--- routes/web.php
+++ routes/web.php
@@ -326,3 +326,8 @@ Route::post('/coupon-apply', [CartController::class, 'CouponApply']);
 Route::get('/coupon-calculation', [CartController::class, 'CouponCalculation']);
 
 Route::get('/coupon-remove', [CartController::class, 'CouponRemove']);
+
+
+ // Checkout Routes 
+
+ Route::get('/checkout', [CartController::class, 'CheckoutCreate'])->name('checkout');
