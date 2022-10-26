008379014f4445b475860e75061c833fabe9b2a7
Abdulrhman-Coder <abdulrhmanalshayay@gmail.com>
2022-10-26 08:35:21 +0300

    27.6.2 Add Cash on Delivery Payment Method

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ use App\Http\Controllers\User\WishlistController;
 use App\Http\Controllers\User\CartPageController;
 use App\Http\Controllers\User\CheckoutController;
 use App\Http\Controllers\User\StripeController;
+use App\Http\Controllers\User\CashController;
+
 use App\Http\Controllers\User\AllUserController;
 
 use App\Http\Controllers\Frontend\IndexController;
@@ -243,6 +245,8 @@ Route::group(['prefix'=>'user','middleware' => ['user','auth'],'namespace'=>'Use
     Route::get('/wishlist-remove/{id}', [WishlistController::class, 'RemoveWishlistProduct']);
 
     Route::post('/stripe/order', [StripeController::class, 'StripeOrder'])->name('stripe.order');
+    
+    Route::post('/cash/order', [CashController::class, 'CashOrder'])->name('cash.order');
 
     Route::get('/my/orders', [AllUserController::class, 'MyOrders'])->name('my.orders');
 
